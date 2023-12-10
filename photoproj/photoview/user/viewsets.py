7f776b20ad41566c6386e894ac685a5e3b1d2441
Ricardo Henrique <ricardosousa339@gmail.com>
2023-12-10 10:51:02 -0300

    Acesso de informacoes do usuário logado funcionando

--- photoproj/photoview/user/viewsets.py
+++ photoproj/photoview/user/viewsets.py
@@ -14,13 +14,13 @@ class UserViewSet(viewsets.ModelViewSet):
     ordering = ['-updated']
 
     def get_queryset(self):
-        if self.request.user.is_superuser:
-            return User.objects.all()
+        return User.objects.filter(id=self.request.user.id)
+
 
     def get_object(self):
         lookup_field_value = self.kwargs[self.lookup_field]
 
-        obj = User.objects.get(lookup_field_value)
+        obj = User.objects.get(id=lookup_field_value)
         self.check_object_permissions(self.request, obj)
 
         return obj
